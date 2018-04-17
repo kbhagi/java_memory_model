c88cf20edf32898996d726d379515110af4bc37e
bhargavak <bhargavak37@gmail.com>
2018-04-17 14:06:40 +0530

    Double-checked singletons don't work

---
+++ Singleton.java
@@ -0,0 +1,13 @@
+class Singleton {
+ private static Singleton INSTANCE  = null;
+ private Singleton()  {....}
+ public static Singleton getInstance() {
+ if (INSTANCE == null)
+ synchronized(Singleton.class) {
+  if (INSTANCE == null)
+     INSTANCE = new Singleton();
+     }
+     return INSTANCE;
+     }
+     ...
+   }  
